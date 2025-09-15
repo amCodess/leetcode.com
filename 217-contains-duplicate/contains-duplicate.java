0dbab03370fcbc9234d6f9252cd0487106a7db95
Amedín <156698560+amCodess@users.noreply.github.com>
2025-09-15 10:08:41 +0200

    Time: 19 ms (37.35%) | Memory: 56.4 MB (98.76%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        boolean isDuplicate = false;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                isDuplicate = true;
+                break;
+            }
+        }
+        return isDuplicate;
+    }
+}
