00bfe3642773d54c0c978908acc590257a009271
Amedín <156698560+amCodess@users.noreply.github.com>
2025-09-16 14:59:40 +0200

    Time: 71 ms (5.96%) | Memory: 54.9 MB (6.81%) - LeetSync

---
+++ java/242-valid-anagram/valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        boolean anagram = true;
+
+        String[] sCharacters = s.split("");
+        String[] tCharacters = t.split("");
+        Arrays.sort(sCharacters);
+        Arrays.sort(tCharacters);
+
+        if(sCharacters.length!=tCharacters.length){
+            anagram = false;
+        }else{
+            for(int i= 0; i<sCharacters.length;i++){
+                if(!sCharacters[i].equals(tCharacters[i])){
+                    anagram = false;
+                    break;
+                }
+            }
+        }
+    
+
+        return anagram;
+    }
+}
